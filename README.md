c5d788c5e95e951228c1416e5b7a34db5d41973d
ZuyThai <duythai2502@gmail.com>
2023-12-12 23:34:49 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# HKDN_Project
+# How to use
+Clone repo
+
+`git clone https://github.com/duytha2502/HKDN_Project.git`
+
+`Save .env.example as .env and put your database credentials`
+
+
+Install the composer dependencies
+
+`composer install`
+
+
+Set application key
+
+`php artisan key:generate`   
+
+And Migrate with
+
+`php artisan migrate --seed` or `php artisan migrate:fresh --seed`
+
+ `php artisan storage:link`
+
+
+Login Credentials for admin panel
+
+ admin@webmall.com | password : password
+ seller1@webmall.com | password : 123
+ seller2@webmall.com | password : 123
---
+++ resources/views/_slider.blade.php
@@ -0,0 +1,18 @@
+<div class="slider-active owl-carousel">
+    <div class="single-slider single-slider-hm3 bg-img pt-170 pb-173"
+        style="background-image: url(assets/img/slider/slider1.jpg)">
+        <div class="slider-animation slider-content-style-3 fadeinup-animated">
+            <h2 class="animated" style="color: white">Invention of <br>design platform</h2>
+            <h4 class="animated" style="color: white">Best Product With warranty </h4>
+            <a class="electro-slider-btn btn-hover" href="product-details.html">buy now</a>
+        </div>
+    </div>
+    <div class="single-slider single-slider-hm3 bg-img pt-170 pb-173"
+        style="background-image: url(assets/img/slider/slider3.jpg)">
+        <div class="slider-animation slider-content-style-3 fadeinup-animated">
+            <h2 class="animated" style="color: white">Invention of <br>design platform</h2>
+            <h4 class="animated" style="color: white">Best Product With warranty </h4>
+            <a class="electro-slider-btn btn-hover" href="product-details.html">buy now</a>
+        </div>
+    </div>
+</div>

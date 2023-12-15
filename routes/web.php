4cb0a5922a4303d1e940fac004195c10113a637f
ZuyThai <duythai2502@gmail.com>
2023-12-16 02:34:12 +0700

    Duy update mail service

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@
 
 Route::redirect('/', '/home');
 
-Auth::routes();
+Auth::routes([
+    'verify' => true
+]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 Route::get('/contact', 'HomeController@contact')->name('contact');
 
 
